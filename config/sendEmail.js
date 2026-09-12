ecc75c590e517c876e9e857f31275eb416772a75
fardeenpathan <md.fardeen5236@gmail.com>
2026-09-12 15:09:04 +0530

    update

--- config/sendEmail.js
+++ config/sendEmail.js
@@ -5,6 +5,10 @@ const transporter = nodemailer.createTransport({
     host:'smtp.gmail.com',
     port:587,
     secure:false,
+
+
+        family: 4,
+        
     connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
